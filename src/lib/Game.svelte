cd9a2772f74efd855c2e3aa86fc5127c5cdcb1a2
Jangis <jangismarifet@gmail.com>
2024-05-01 10:25:59 -0400

    first commit

---
+++ src/lib/Game.svelte
@@ -0,0 +1,31 @@
+<script>
+    import Square from "./Square.svelte";
+
+    let currentPosition = 0;
+    let letters = ["Q", "", "", "", ""]; // 5 squares
+
+    function moveLeft() {
+        if (currentPosition > 0) {
+            currentPosition--;
+        }
+    }
+
+    function moveRight() {
+        if (currentPosition < letters.length - 1) {
+            currentPosition++;
+        }
+    }
+</script>
+
+<div>
+    <div>
+        {#each letters as letter, i}
+            <Square {letter} {currentPosition} index={i} />
+        {/each}
+    </div>
+    <br /><br /><br /><br /><br /><br />
+    <div>
+        <button on:click={moveLeft}>Left Arrow</button>
+        <button on:click={moveRight}>Right Arrow</button>
+    </div>
+</div>
---
+++ src/lib/Square.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let letter;
+    export let currentPosition;
+    export let index;
+
+    let translateY = 0;
+
+    $: {
+        if (index < currentPosition) {
+            translateY = -50;
+        } else if (index > currentPosition) {
+            translateY = 50;
+        } else {
+            translateY = 0;
+        }
+    }
+</script>
+
+<div class="square" style="transform: translateY({translateY}px);">
+    {letter}
+</div>
+
+<style>
+    .square {
+        width: 50px;
+        height: 50px;
+        border: 1px solid black;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 24px;
+        transition: transform 0.7s ease;
+    }
+</style>
